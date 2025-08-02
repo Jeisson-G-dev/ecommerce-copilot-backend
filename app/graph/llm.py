a489b4a78b7ef4a15034fa3d6a3b2fdb0d022d07
unknown <nicop4643@gmail.com>
2025-08-02 03:07:15 -0500

    Ajuste de credenciales cloud

--- app/graph/llm.py
+++ app/graph/llm.py
@@ -1,8 +1,15 @@
+import os
+import json
+from google.oauth2 import service_account
 from langchain_google_genai import ChatGoogleGenerativeAI
-from app.config import config
+from config import config
 
-# Gemini LLM desde LangChain
+# Cargar credenciales desde la variable de entorno
+credentials_dict = json.loads(os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"])
+credentials = service_account.Credentials.from_service_account_info(credentials_dict)
+
 llm = ChatGoogleGenerativeAI(
     model=config.GEMINI_MODEL,
-    temperature=0
+    temperature=0,
+    credentials=credentials
 )
